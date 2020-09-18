ee01df539a10f8b562d773fa4e0176a380d01d5a
Athlon1600 <Athlon2400@gmail.com>
2020-09-18 13:56:52 -0500

    updated build logic + updated prebuilt dist js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,10 @@
 let mix = require('laravel-mix');
 
-mix.js('resources/js/field.js', 'dist/js').webpackConfig({
-  resolve: {
-    symlinks: false
-  }
+// mimic build logic as is generated when you do nova:field
+// because otherwise you get this issue:
+// https://github.com/JeffreyWay/laravel-mix/issues/1126#issuecomment-325581328
+mix.setPublicPath('dist').js('resources/js/field.js', 'js').webpackConfig({
+    resolve: {
+        symlinks: false
+    }
 });
